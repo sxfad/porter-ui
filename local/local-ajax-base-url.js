2e1e0001b94e7656e9b39e18fcff87392bbc6270
zhangkewei <zhangkewei@sina.cn>
2018-08-01 18:55:43 +0800

    修改默认配置

--- local/local-ajax-base-url.js
+++ local/local-ajax-base-url.js
@@ -1,27 +1,4 @@
 /*
  * 开发模式下ajax base url 单独提出文件，并git ignore，防止开发人员之间冲突
  * */
-// export default 'http://172.16.136.79:8080/';
-// export default 'http://172.16.40.48:8090';
-// export default 'https://172.16.136.85/api/';
-// export default 'http://172.16.136.37:8080/';
-
-//export default 'http://172.16.135.30:8080/api/manager'; //开发
-export default 'http://172.16.154.5:8080/api/manager'; //測試
-// export default 'http://172.16.41.168:8080'; //郭洪健
-// export default 'http://172.16.42.107:8080/api/manager'; //付紫钲
-
-//
-/*
- *
- return 'http://172.16.135.168:8080/';
- // return 'http://172.16.40.48:8090';
- // return 'http://172.16.41.157:8080/';
- // return 'http://172.16.40.50:8081/';
- // return 'http://172.16.20.57:8081';
- // return 'http://172.16.40.53:8090/';
- * */
-
-// http://172.16.42.107:8080/swagger-ui.html
-
-//GET /api/topTraceListDataLoadGET /api/topTraceListDataLoad#/
+ return 'http://127.0.0.1:8080/';
