27c4fa7e770036fb6b4dd1f962d976317c20c6f3
张科伟 <zhang_kw@suixingpay.com>
2018-10-22 15:17:11 +0800

    调整客户端地址

--- ajax-config.js
+++ ajax-config.js
@@ -1,6 +1,6 @@
 export default  {
 	'production': '/api/manager',
-	'test': 'http://172.16.154.5:8080/api/manager',
+	'test': 'http://172.16.154.162:8080/api/manager',
 	'rc': '/api/manager',
 	'dev': 'http://172.16.135.30:8080/api/manager'
 }
